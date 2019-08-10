188d521f55f8ae0ee55564c1f191e415d7b76ee5
ryotank7 <eight.game715@gamil.com>
2019-08-10 14:54:42 +0900

    fitst commit

---
+++ vender/bundle/bin/chromedriver-update.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"C:\Ruby\devkit\bin\ruby.exe" "C:/Users/cakee/Desktop/work/cocoon_sample/vender/bundle/bin/chromedriver-update" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"C:\Ruby\devkit\bin\ruby.exe" "%~dpn0" %*
